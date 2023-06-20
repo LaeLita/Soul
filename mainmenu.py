f7ed9f4ea74873d0b22a5a864f7db76d814962a7
LaeLita <50533531+LaeLita@users.noreply.github.com>
2023-06-19 22:10:23 -0700

    Update mainmenu.py

--- mainmenu.py
+++ mainmenu.py
@@ -1,5 +1,6 @@
-import os
 import pygame
+import os
+from PIL import Image
 
 # Initialize Pygame
 pygame.init()
@@ -11,71 +12,43 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Horse Game")
 
 def main_menu():
-    # Load the background image
+    # Load and display the background image
     image_filename = "mainbackground.png"
     image_path = os.path.join("gameimages", image_filename)
     background_image = pygame.image.load(image_path).convert()
+    screen.blit(background_image, (0, 0))
+
+    # Create a button for the new game
+    new_game_button = pygame.Rect(100, 200, 200, 50)
 
     # Main menu loop
     running = True
     while running:
-        # Clear the screen
-        screen.fill((0, 0, 0))
-
-        # Draw the background image
-        screen.blit(background_image, (0, 0))
-
-        # Draw the menu options
-        menu_font = pygame.font.Font(None, 32)
-        text = menu_font.render("===== Horse Game =====", True, (255, 255, 255))
-        screen.blit(text, (screen_width // 2 - text.get_width() // 2, 50))
-
-        menu_options = ["1. New Game", "2. Load Game", "3. Settings", "4. Exit Game"]
-        for i, option in enumerate(menu_options):
-            text = menu_font.render(option, True, (255, 255, 255))
-            screen.blit(text, (screen_width // 2 - text.get_width() // 2, 150 + i * 50))
-
-        # Update the display
-        pygame.display.flip()
-
-        # Process events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_1:
-                    new_game()
-                elif event.key == pygame.K_2:
-                    load_game()
-                elif event.key == pygame.K_3:
-                    settings()
-                elif event.key == pygame.K_4:
-                    exit_game()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:  # Left mouse button
+                    mouse_pos = pygame.mouse.get_pos()
+                    if new_game_button.collidepoint(mouse_pos):
+                        cutscene()
+                        character_creator()
 
-    # Clean up
-    pygame.quit()
+        # Draw the buttons
+        pygame.draw.rect(screen, (255, 0, 0), new_game_button)
 
-def new_game():
-    # Add your code for starting a new game here
-    print("Starting a new game...")
-    # Rest of the code for the new game
+        # Update the screen
+        pygame.display.flip()
 
-def load_game():
-    # Add your code for loading a game here
-    print("Loading a game...")
-    # Rest of the code for loading the game
+def cutscene():
+    # Add your code for the cutscene here
+    print("Cutscene...")
+    # Rest of the code for the cutscene
 
-def settings():
-    # Add your code for the settings menu here
-    print("Settings menu...")
-    # Rest of the code for the settings menu
-
-def exit_game():
-    print("Exiting the game...")
-    # Add any necessary cleanup code here
-    # Exiting the program
-    pygame.quit()
-    exit()
+def character_creator():
+    # Add your code for the character creator here
+    print("Character creator...")
+    # Rest of the code for the character creator
 
 # Entry point of the program
 if __name__ == "__main__":
