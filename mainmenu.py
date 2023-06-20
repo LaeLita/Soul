348fb68a73227d945701bff2f570c688a57b8970
LaeLita <50533531+LaeLita@users.noreply.github.com>
2023-06-19 22:06:24 -0700

    Update mainmenu.py

--- mainmenu.py
+++ mainmenu.py
@@ -1,31 +1,59 @@
 import os
-from PIL import Image
+import pygame
+
+# Initialize Pygame
+pygame.init()
+
+# Set up the screen
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Horse Game")
 
 def main_menu():
-    # Load and display the background image
+    # Load the background image
     image_filename = "mainbackground.png"
     image_path = os.path.join("gameimages", image_filename)
-    background_image = Image.open(image_path)
-    background_image.show()
+    background_image = pygame.image.load(image_path).convert()
 
-    print("===== Horse Game =====")
-    print("1. New Game")
-    print("2. Load Game")
-    print("3. Settings")
-    print("4. Exit Game")
+    # Main menu loop
+    running = True
+    while running:
+        # Clear the screen
+        screen.fill((0, 0, 0))
 
-    choice = input("Enter your choice (1-4): ")
-    if choice == "1":
-        new_game()
-    elif choice == "2":
-        load_game()
-    elif choice == "3":
-        settings()
-    elif choice == "4":
-        exit_game()
-    else:
-        print("Invalid choice. Please try again.")
-        main_menu()
+        # Draw the background image
+        screen.blit(background_image, (0, 0))
+
+        # Draw the menu options
+        menu_font = pygame.font.Font(None, 32)
+        text = menu_font.render("===== Horse Game =====", True, (255, 255, 255))
+        screen.blit(text, (screen_width // 2 - text.get_width() // 2, 50))
+
+        menu_options = ["1. New Game", "2. Load Game", "3. Settings", "4. Exit Game"]
+        for i, option in enumerate(menu_options):
+            text = menu_font.render(option, True, (255, 255, 255))
+            screen.blit(text, (screen_width // 2 - text.get_width() // 2, 150 + i * 50))
+
+        # Update the display
+        pygame.display.flip()
+
+        # Process events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1:
+                    new_game()
+                elif event.key == pygame.K_2:
+                    load_game()
+                elif event.key == pygame.K_3:
+                    settings()
+                elif event.key == pygame.K_4:
+                    exit_game()
+
+    # Clean up
+    pygame.quit()
 
 def new_game():
     # Add your code for starting a new game here
@@ -46,6 +74,7 @@ def exit_game():
     print("Exiting the game...")
     # Add any necessary cleanup code here
     # Exiting the program
+    pygame.quit()
     exit()
 
 # Entry point of the program
