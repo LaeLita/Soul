cd0393dbb4bd65920d020e6bb729bee404349d69
LaeLita <50533531+LaeLita@users.noreply.github.com>
2023-06-19 23:09:03 -0700

    Update game.py

--- game.py
+++ game.py
@@ -1,9 +1,16 @@
 import mainmenu
+import pygame
 
 def mainmenu():
     # Your code for the main menu goes here
     # ...
 
+    pygame.event.wait()  # Wait for any key press
+
+    pygame.quit()
+    mainmenu.main_menu()  # Call the main_menu() function from mainmenu.py
+
 if __name__ == "__main__":
     mainmenu()
+
 
